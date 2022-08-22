854e87bea9d8c222a68302a3d2899d600ac65418
AditiA13 <110167489+aditi13a@users.noreply.github.com>
2022-08-22 04:18:36 +0000

    First

--- p0original.c
+++ p0original.c
@@ -1,1 +1,32 @@
- 
+#include <stdio.h>
+int input()
+{
+  int a;
+  printf("Enter a number");
+  scanf("%d",&a);
+  return a;
+}
+
+int find_largest(int a, int b, int c)
+{
+  int d;
+  if (a < b)
+    d = b;
+  else
+    d = a;
+  return 
+}
+
+int output(int a, int b, int c, int d)
+{
+  printf("largest of %d and %d is %d",a,b,c);
+}
+
+int main()
+{
+  int a, b, c;
+  a=input();
+  b=input();
+  c=find_largest(a,b);
+  output(a,b,c);
+} 
